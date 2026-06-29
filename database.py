994bfb6fbff1c64039c3d1ef370884fe0ba59b16
Chaithanya-Aiml <chaithanyahu3028@gmail.com>
2026-06-29 14:07:21 +0530

    fastapinotes

--- database.py
+++ database.py
@@ -1,20 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:chaithanya@localhost:5432/student_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()   # <-- Must have ()
+SQLALCHEMY_DATABASE_URL="postgresql://postgres:sandhya@localhost:5432/student_db"
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db=SessionLocal()
     try:
         yield db
     finally:
---
+++ frontend/test.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+// let x=10;
+// console.log(x);
+// const y=20;
+// console.log(y);
+// console.log(x+y);
+function add(x,y){
+    return x+y;
+
+}
+console.log(add(10,20));
