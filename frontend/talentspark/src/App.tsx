b6481991b167bc6a1c0716a97f05872a2fbe7a06
Chaithanya-Aiml <chaithanyahu3028@gmail.com>
2026-06-30 13:47:44 +0530

    fastapinotes

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -3,13 +3,44 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
+import type {Company} from "./types/company";
+import {useEffect, useState} from "react";
+import {getCompanies} from "./Services/CompanyService";
 
 function App(){
+  const [loading,setLoading] = useState(true);
+  const [companies,setCompanies] = useState<Company[]>([]);
+  const [error,setError] = useState<string | null>(null);
+
+  async function fetchCompanies() {
+    setLoading(true);
+    try {
+      const companies = await getCompanies();
+      setCompanies(companies);
+    } catch (error) {
+      setError("error");
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return(
     <>
     <Welcome/>
     <NavBar/>
-    <CompanyCard/>
+    <br/>
+
+    <CompanyCard key={companies.id}
+    companies={companies}/>
+
     <JobCard/>
     <Footer/>
     </>
--- frontend/talentspark/src/components/Welcome.tsx
+++ frontend/talentspark/src/components/Welcome.tsx
@@ -1,5 +1,16 @@
-function Welcome() {
-  return <h1>Welcome To TalentSpark</h1>
-}
+import {useState} from "react"
 
-export default Welcome
+function Counter() {
+    const [count, setCount] = useState(0)
+    const increment = () => {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+          <h1>Count:{count}</h1>
+          <button onClick={increment}>Increment</button>
+        </div>
+      )
+    }
+    export default Counter
+
